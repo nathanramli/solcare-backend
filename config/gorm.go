bf69f50792aa4de38242873de9b3fb97b686a7bc
Nathan Ramli <hezkyanatanael84@gmail.com>
2023-01-14 12:58:30 +0700

    chore: fix ci cd

--- config/gorm.go
+++ config/gorm.go
@@ -10,11 +10,11 @@ import (
 )
 
 func ConnectPostgresGORM() (*gorm.DB, error) {
-	host := os.Getenv("PGHOST")
-	port := os.Getenv("PGPORT")
-	user := os.Getenv("PGUSER")
-	pass := os.Getenv("PGPASSWORD")
-	name := os.Getenv("PGDATABASE")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	name := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
