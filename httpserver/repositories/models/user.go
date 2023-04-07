62234c802509f06e154384dbf10987a2768ad847
Nathan Ramli <hezkyanatanael84@gmail.com>
2023-04-07 20:17:40 +0700

    fix: set default value to bool fields

--- httpserver/repositories/models/user.go
+++ httpserver/repositories/models/user.go
@@ -8,9 +8,9 @@ type Users struct {
 	Email          string
 	FirstName      string
 	LastName       string
-	Gender         *bool
-	IsVerified     *bool
-	IsWarned       *bool
+	Gender         *bool `gorm:"default:true"`
+	IsVerified     *bool `gorm:"default:false"`
+	IsWarned       *bool `gorm:"default:false"`
 	IdCardNumber   string
 	ProfilePicture string
 }
